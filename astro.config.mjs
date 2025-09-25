8c2250aef2e168d1fdbd65b0a8bdbb44b4345ba3
Kevin Christ <christkevin50@gmail.com>
2025-09-25 20:29:33 +0200

    updated the default site location for gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kevinchrist02.github.io',
+    base: '/blog'
 	integrations: [mdx(), sitemap()],
 });
